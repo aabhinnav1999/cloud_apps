175c27b1766706265614ec4784b0a52e22997005
aabhinnav1999 <vdabhinavkotagiri@gmail.com>
2026-03-22 11:20:04 +0000

    updated files and added docker-compose.yml

--- ecommerce/user-service/src/main/java/com/ecommerce/userservice/config/SecurityConfig.java
+++ ecommerce/user-service/src/main/java/com/ecommerce/userservice/config/SecurityConfig.java
@@ -2,14 +2,16 @@ package com.ecommerce.userservice.config;
 
 import com.ecommerce.userservice.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.*;
-import org.springframework.security.web.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
